2f8a7411e7a901366a95ab1f8b2629ed18924117
Kumar Saksham <everydaycodings@gmail.com>
2024-09-17 15:55:52 +0530

    added azure database and filestorage

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class StaticAssets(models.Model):
+    reference_name = models.CharField(null=True, blank=True)
+    name = models.CharField(null=True, blank=True)
+    value = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Static Asset"  # Singular form
+        verbose_name_plural = "Static Assets"  # Plural form
--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,16 @@
+from lib2to3.fixes.fix_input import context
+
+from decouple import config
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect, render
+from .models import StaticAssets
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+
+    profile = get_object_or_404(StaticAssets, reference_name="profile")
+
+    context = {
+        "profile": profile
+    }
+
+    return render(request, "index.html", context=context)
