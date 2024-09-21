c2e851ac2ffd38808493fcfc574ae9cca34070fe
Kumar Saksham <everydaycodings@gmail.com>
2024-09-21 06:17:33 +0530

    added data value for index page

--- resume/views.py
+++ resume/views.py
@@ -8,9 +8,37 @@ from .models import StaticAssets
 def index(request):
 
     profile = get_object_or_404(StaticAssets, reference_name="profile")
+    experience = get_object_or_404(StaticAssets, reference_name="years_experience")
+    projects = get_object_or_404(StaticAssets, reference_name="projects_completed")
+    tech = get_object_or_404(StaticAssets, reference_name="known_tech")
+    commits = get_object_or_404(StaticAssets, reference_name="code_commits")
+    profession = get_object_or_404(StaticAssets, reference_name="profession")
+    bio = get_object_or_404(StaticAssets, reference_name="index_bio")
+    twitter = get_object_or_404(StaticAssets, reference_name="twitter")
+    linkedin = get_object_or_404(StaticAssets, reference_name="linkedin")
+    github = get_object_or_404(StaticAssets, reference_name="github")
+    tag1 = get_object_or_404(StaticAssets, reference_name="tag1")
+    tag2 = get_object_or_404(StaticAssets, reference_name="tag2")
+    tag3 = get_object_or_404(StaticAssets, reference_name="tag3")
+    tag4 = get_object_or_404(StaticAssets, reference_name="tag4")
+    resume = get_object_or_404(StaticAssets, reference_name="resume")
 
     context = {
-        "profile": profile
+        "profile": profile,
+        "experience": experience,
+        "projects": projects,
+        "tech": tech,
+        "commits": commits,
+        "profession": profession,
+        "bio": bio,
+        "twitter": twitter,
+        "linkedin": linkedin,
+        "github": github,
+        "tag1": tag1,
+        "tag2": tag2,
+        "tag3": tag3,
+        "tag4": tag4,
+        "resume": resume,
     }
 
     return render(request, "index.html", context=context)
